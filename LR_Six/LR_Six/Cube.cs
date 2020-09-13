671d98260b4907352a8cfa3397af5d79191f6cb9
Alexey Svidnitskiy <lesha.svid@yandex.ru>
2020-09-13 05:35:49 +0300

    Лабораторная работа 6. Классы. Объектное моделирование

---
+++ LR_Six/LR_Six/Cube.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LR_Six
+{
+    class Cube
+    {
+        /* Класс «Куб». Реализовать ввод и вывод полей данных, вычисление
+         * объема, площади поверхности, а также вывод информации об объекте.
+         */
+        private double a;
+
+        public double GetV()
+        {
+            return (Math.Pow(a, 3));
+        }
+
+        public double GetS()
+        {
+            return (6 * Math.Pow(a, 2));
+        }
+
+        public void Info ()
+        {
+#if DEBUG
+            Console.WriteLine();
+#endif
+            String str =
+                "***************\n" +
+                "*             *\n" +
+                "*             *\n" +
+                "*     Куб     *\n" +
+                "*             *\n" +
+                "*             *\n" +
+                "***************\n";
+            Console.WriteLine(str);
+            Console.WriteLine("Результаты следующие:");
+            Console.WriteLine(string.Format("V = {0:0.000}", GetV()));
+            Console.WriteLine(string.Format("S = {0:0.000}", GetS()));
+        }
+
+        public void Load()
+        {
+            a = Convert.ToDouble(Console.ReadLine());
+        }
+    }
+}
---
+++ LR_Six/LR_Six/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LR_Six
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вариант 27
+
+#if DEBUG
+            Console.Write("Введите данные для стороны куба. A: ");
+#endif
+
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"cube_output.txt");
+            var new_in = new StreamReader(@"cube_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Cube c = new Cube();
+            c.Load();
+            c.Info();
+
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#endif
+        }
+    }
+}
